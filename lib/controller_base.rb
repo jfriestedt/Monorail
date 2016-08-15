8fcd79ba915546a07c891b5a1fb40609ba3c70f0
Jon Friestedt <jfrieste@gmail.com>
2016-08-15 16:46:10 -0400

    Initial Commit - build rails lite

---
+++ lib/controller_base.rb
@@ -0,0 +1,67 @@
+require 'active_support'
+require 'active_support/core_ext'
+require 'erb'
+require_relative './session'
+
+class ControllerBase
+  attr_reader :req, :res, :params
+
+  # Setup the controller
+  def initialize(req, res, params = {})
+    @req = req
+    @res = res
+    @params = params
+    @already_built_response = false
+  end
+
+  # Helper method to alias @already_built_response
+  def already_built_response?
+    @already_built_response
+  end
+
+  # Set the response status code and header
+  def redirect_to(url)
+    raise 'Double render all the way across the sky! What does it mean?' if already_built_response?
+    @res.set_header('Location', url)
+    @res.status = 302
+    session.store_session(@res)
+    @already_built_response = true
+  end
+
+  # Populate the response with content.
+  # Set the response's content type to the given type.
+  # Raise an error if the developer tries to double render.
+  def render_content(content, content_type)
+    raise 'Double render all the way across the sky! What does it mean?' if already_built_response?
+    @res['Content-Type'] = content_type
+    @res.write(content)
+    session.store_session(@res)
+    @already_built_response = true
+  end
+
+  # use ERB and binding to evaluate templates
+  # pass the rendered html to render_content
+  def render(template_name)
+    raise 'Double render all the way across the sky! What does it mean?' if already_built_response?
+    path = "views/#{self.class.name.underscore}/#{template_name}.html.erb"
+    file = File.read(path)
+    erb = ERB.new(file).result(binding)
+    render_content(erb, 'text/html')
+    @already_built_response = true
+  end
+
+  # method exposing a `Session` object
+  def session
+    @session ||= Session.new(req)
+  end
+
+  def show
+    @cat = Cat.find(params[:id])
+  end
+
+  # use this with the router to call action_name (:index, :show, :create...)
+  def invoke_action(name)
+    self.send(name)
+    render(name) unless already_built_response?
+  end
+end
