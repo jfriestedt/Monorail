7dbff674e902b8a598f111622cfddea087d0a3b9
Jon Friestedt <jfrieste@gmail.com>
2016-08-16 11:34:22 -0400

    Remove comments

--- lib/session.rb
+++ lib/session.rb
@@ -2,8 +2,6 @@ require 'json'
 require 'byebug'
 
 class Session
-  # find the cookie for this app
-  # deserialize the cookie into a hash
   def initialize(req)
     @session_cookie = req.cookies['_rails_lite_app']
 
@@ -18,8 +16,6 @@ class Session
     @parsed_cookie[key] = val
   end
 
-  # serialize the hash into json and save in a cookie
-  # add to the responses cookies
   def store_session(res)
     res.set_cookie(
       '_rails_lite_app',
