eb99fa42a2ac417f4a128525e9dd40164c266458
Jon Friestedt <jfrieste@gmail.com>
2016-08-16 11:29:26 -0400

    Remove development files

--- lib/controller_base.rb
+++ lib/controller_base.rb
@@ -6,7 +6,6 @@ require_relative './session'
 class ControllerBase
   attr_reader :req, :res, :params
 
-  # Setup the controller
   def initialize(req, res, params = {})
     @req = req
     @res = res
@@ -14,12 +13,10 @@ class ControllerBase
     @already_built_response = false
   end
 
-  # Helper method to alias @already_built_response
   def already_built_response?
     @already_built_response
   end
 
-  # Set the response status code and header
   def redirect_to(url)
     raise 'Double render all the way across the sky! What does it mean?' if already_built_response?
     @res.set_header('Location', url)
@@ -28,9 +25,6 @@ class ControllerBase
     @already_built_response = true
   end
 
-  # Populate the response with content.
-  # Set the response's content type to the given type.
-  # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
     raise 'Double render all the way across the sky! What does it mean?' if already_built_response?
     @res['Content-Type'] = content_type
@@ -39,8 +33,6 @@ class ControllerBase
     @already_built_response = true
   end
 
-  # use ERB and binding to evaluate templates
-  # pass the rendered html to render_content
   def render(template_name)
     raise 'Double render all the way across the sky! What does it mean?' if already_built_response?
     path = "views/#{self.class.name.underscore}/#{template_name}.html.erb"
@@ -50,7 +42,6 @@ class ControllerBase
     @already_built_response = true
   end
 
-  # method exposing a `Session` object
   def session
     @session ||= Session.new(req)
   end
@@ -59,7 +50,6 @@ class ControllerBase
     @cat = Cat.find(params[:id])
   end
 
-  # use this with the router to call action_name (:index, :show, :create...)
   def invoke_action(name)
     self.send(name)
     render(name) unless already_built_response?
