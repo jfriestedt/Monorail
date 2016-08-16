07de9cb84460421a57780b9a2f585be5061c7d93
Jon Friestedt <jfrieste@gmail.com>
2016-08-16 12:06:51 -0400

    Refactor session.rb

--- lib/session.rb
+++ lib/session.rb
@@ -2,24 +2,25 @@ require 'json'
 require 'byebug'
 
 class Session
-  def initialize(req)
-    @session_cookie = req.cookies['_rails_lite_app']
+  attr_accessor :parsed_cookie
 
-    @parsed_cookie = @session_cookie ? JSON.parse(@session_cookie) : {}
+  def initialize(req)
+    session_cookie = req.cookies['_monorail_app']
+    @parsed_cookie = session_cookie ? JSON.parse(@session_cookie) : {}
   end
 
   def [](key)
-    @parsed_cookie[key]
+    parsed_cookie[key]
   end
 
   def []=(key, val)
-    @parsed_cookie[key] = val
+    parsed_cookie[key] = val
   end
 
   def store_session(res)
     res.set_cookie(
-      '_rails_lite_app',
-      {path: '/', value: @parsed_cookie.to_json}
+      '_monorail_app',
+      {path: '/', value: parsed_cookie.to_json}
     )
   end
 end
