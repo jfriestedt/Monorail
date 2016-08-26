0bcb615962a6a04236883fbdd0aa31cad047e11b
Jon Friestedt <jfrieste@gmail.com>
2016-08-26 14:06:21 -0400

    Add module Monorail wrappers

--- lib/route.rb
+++ lib/route.rb
@@ -1,27 +1,29 @@
-class Route
-  attr_reader :pattern, :http_method, :controller_class, :action_name
+Module Monorail
+  class Route
+    attr_reader :pattern, :http_method, :controller_class, :action_name
 
-  def initialize(pattern, http_method, controller_class, action_name)
-    @pattern = pattern
-    @http_method = http_method
-    @controller_class = controller_class
-    @action_name = action_name
-  end
+    def initialize(pattern, http_method, controller_class, action_name)
+      @pattern = pattern
+      @http_method = http_method
+      @controller_class = controller_class
+      @action_name = action_name
+    end
 
-  def matches_request?(req)
-    (pattern =~ req.path) &&
-    (http_method == req.request_method.downcase.to_sym)
-  end
+    def matches_request?(req)
+      (pattern =~ req.path) &&
+      (http_method == req.request_method.downcase.to_sym)
+    end
 
-  def run(req, res)
-    match_data = pattern.match(req.path)
+    def run(req, res)
+      match_data = pattern.match(req.path)
 
-    params = req.params
-    param_keys = match_data.names
+      params = req.params
+      param_keys = match_data.names
 
-    param_keys.each { |key| params[key] = match_data[key] }
+      param_keys.each { |key| params[key] = match_data[key] }
 
-    controller = controller_class.new(req, res, params)
-    controller.invoke_action(action_name)
+      controller = controller_class.new(req, res, params)
+      controller.invoke_action(action_name)
+    end
   end
 end
--- lib/router.rb
+++ lib/router.rb
@@ -1,39 +1,41 @@
 require_relative ('./route')
 
-class Router
-  attr_reader :routes
+Module Monorail
+  class Router
+    attr_reader :routes
 
-  def initialize
-    @routes = []
-  end
-
-  def add_route(pattern, method, controller_class, action_name)
-    route = Route.new(pattern, method, controller_class, action_name)
-    @routes << route
-  end
-
-  def draw(&proc)
-    self.instance_eval(&proc)
-  end
-
-  [:get, :post, :put, :delete].each do |http_method|
-    define_method(http_method) do |pattern, controller_class, action_name|
-      add_route(pattern, http_method, controller_class, action_name)
+    def initialize
+      @routes = []
     end
-  end
 
-  def match_req_to_route(req)
-    @routes.find { |route| route.matches_request?(req) }
-  end
+    def add_route(pattern, method, controller_class, action_name)
+      route = Route.new(pattern, method, controller_class, action_name)
+      @routes << route
+    end
 
-  def run(req, res)
-    route = match_req_to_route(req)
+    def draw(&proc)
+      self.instance_eval(&proc)
+    end
 
-    if route
-      route.run(req, res)
-    else
-      res.write("Your request failed.")
-      res.status = 404
+    [:get, :post, :put, :delete].each do |http_method|
+      define_method(http_method) do |pattern, controller_class, action_name|
+        add_route(pattern, http_method, controller_class, action_name)
+      end
+    end
+
+    def match_req_to_route(req)
+      @routes.find { |route| route.matches_request?(req) }
+    end
+
+    def run(req, res)
+      route = match_req_to_route(req)
+
+      if route
+        route.run(req, res)
+      else
+        res.write("Your request failed.")
+        res.status = 404
+      end
     end
   end
 end
